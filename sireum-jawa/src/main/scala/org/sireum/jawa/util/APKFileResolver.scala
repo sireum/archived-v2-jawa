086b312b05399dbceac5fd132fd5d99e026cd2d4
fgwei <wfg611004900521@gmail.com>
2015-01-28 14:42:55 -0800

    add isZipFile method

--- sireum-jawa/src/main/scala/org/sireum/jawa/util/APKFileResolver.scala
+++ sireum-jawa/src/main/scala/org/sireum/jawa/util/APKFileResolver.scala
@@ -17,6 +17,9 @@ import java.io.InputStream
 import org.sireum.util.FileResourceUri
 import java.io.FileInputStream
 import java.net.URI
+import java.io.IOException
+import java.io.DataInputStream
+import java.io.BufferedInputStream
 
 /**
  * @author <a href="mailto:fgwei@k-state.edu">Fengguo Wei</a>
@@ -24,11 +27,31 @@ import java.net.URI
 object APKFileResolver {
   
   /**
+   * Determine whether a file is a ZIP File.
+   */
+  def isZipFile(file : File) : Boolean = {
+      if(file.isDirectory()) {
+          return false
+      }
+      if(!file.canRead()) {
+          throw new IOException("Cannot read file "+file.getAbsolutePath())
+      }
+      if(file.length() < 4) {
+          return false
+      }
+      val in = new DataInputStream(new BufferedInputStream(new FileInputStream(file)))
+      val test = in.readInt()
+      in.close()
+      return test == 0x504b0304
+  }
+  
+  /**
    * given an APK file uri, the following method returns the uri of the inner dex file
    */
 	def getDexFile(apkUri : FileResourceUri, outputUri : FileResourceUri) : FileResourceUri = {
     //create directory
 	  val apkFile = new File(new URI(apkUri))
+    if(!isZipFile(apkFile)) throw new RuntimeException("File "+ apkFile.getAbsolutePath() + " is not a zip File!")
 	  val zipis = new ZipInputStream(new FileInputStream(apkFile))
     val dirName = apkFile.getName().substring(0, apkFile.getName().lastIndexOf("."))
     val outputDir = new File(new URI(outputUri + "/" + dirName))
