fff5457d62026d0035fd6e0b0763dc278b7097a3
fgwei <wfg611004900521@gmail.com>
2015-05-05 19:08:34 -0700

    fix bugs

---
+++ sireum-jawa-alir/src/main/scala/org/sireum/jawa/alir/pta/reachingFactsAnalysis/model/ConstructorModel.scala
@@ -0,0 +1,28 @@
+package org.sireum.jawa.alir.pta.reachingFactsAnalysis.model
+
+import org.sireum.jawa.JawaMethod
+import org.sireum.jawa.Center
+import org.sireum.jawa.alir.pta.PTAResult
+import org.sireum.jawa.alir.pta.reachingFactsAnalysis.RFAFact
+import org.sireum.jawa.alir.Context
+import org.sireum.util._
+import org.sireum.jawa.alir.Context
+import org.sireum.jawa.ScopeManager
+
+object ConstructorModel {
+  val TITLE = "ConstructorModel"
+  def isConstructor(m: JawaMethod): Boolean = {
+    val res = ScopeManager.getCurrentScopeManager.shouldBypass(m.getDeclaringClass) &&
+    m.getName.contains(m.getDeclaringClass.constructorName)
+    res
+  }
+  
+  def doConstructorCall(s: PTAResult, p: JawaMethod, args: List[String], retVars: Seq[String], currentContext: Context): (ISet[RFAFact], ISet[RFAFact], Boolean) = {
+    var newFacts = isetEmpty[RFAFact]
+    var delFacts = isetEmpty[RFAFact]
+    var byPassFlag = true
+    val typs = p.getParamTypes
+    if(typs.size == 1) byPassFlag = false
+    (newFacts, delFacts, byPassFlag)
+  }
+}
---
+++ sireum-jawa-alir/src/main/scala/org/sireum/jawa/alir/pta/reachingFactsAnalysis/model/ObjectModel.scala
@@ -0,0 +1,56 @@
+/*
+Copyright (c) 2013-2014 Fengguo Wei & Sankardas Roy, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+package org.sireum.jawa.alir.pta.reachingFactsAnalysis.model
+
+import org.sireum.jawa._
+import org.sireum.util._
+import org.sireum.jawa.alir.pta.reachingFactsAnalysis._
+import org.sireum.jawa.alir.Context
+import org.sireum.jawa.util.StringFormConverter
+import org.sireum.jawa.MessageCenter._
+import org.sireum.jawa.alir.JawaAlirInfoProvider
+import org.sireum.jawa.alir.pta._
+
+/**
+ * @author <a href="mailto:fgwei@k-state.edu">Fengguo Wei</a>
+ */ 
+object ObjectModel {
+  val TITLE = "ObjectModel"
+	def isObject(r : JawaClass) : Boolean = r.getName == "java.lang.Object"
+	  
+	def doObjectCall(s : PTAResult, p : JawaMethod, args : List[String], retVars : Seq[String], currentContext : Context) : (ISet[RFAFact], ISet[RFAFact], Boolean) = {
+	  var newFacts = isetEmpty[RFAFact]
+	  var delFacts = isetEmpty[RFAFact]
+	  var byPassFlag = true
+	  p.getSignature match{
+	    case "Ljava/lang/Object;.getClass:()Ljava/lang/Class;" =>
+        require(retVars.size == 1)
+        objectGetClass(s, args, retVars(0), currentContext) match{case (n, d) => newFacts ++= n; delFacts ++= d}
+        byPassFlag = false
+      case _ =>
+	  }
+	  (newFacts, delFacts, byPassFlag)
+	}
+	
+	private def objectGetClass(s : PTAResult, args : List[String], retVar : String, currentContext : Context): (ISet[RFAFact], ISet[RFAFact]) = {
+    require(args.size > 0)
+    val thisSlot = VarSlot(args(0))
+    val thisValue = s.pointsToSet(thisSlot, currentContext)
+    var newfacts = isetEmpty[RFAFact]
+    var delfacts = isetEmpty[RFAFact]
+    thisValue.foreach{
+      cIns =>
+        val name = cIns.typ.name
+        val strIns = ClassInstance(name, cIns.defSite)
+        newfacts += (RFAFact(VarSlot(retVar), strIns))
+    }
+    (newfacts, delfacts)
+  }
+	
+	
+}
