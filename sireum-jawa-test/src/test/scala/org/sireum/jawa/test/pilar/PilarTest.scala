dd7932d9bee32a0e23643bfaaba64620d58df060
fgwei <wfg611004900521@gmail.com>
2015-11-24 03:28:38 -0500

    commit out some test

--- sireum-jawa-test/src/test/scala/org/sireum/jawa/test/pilar/PilarTest.scala
+++ sireum-jawa-test/src/test/scala/org/sireum/jawa/test/pilar/PilarTest.scala
@@ -1,25 +1,25 @@
-/*
-Copyright (c) 2013-2014 Fengguo Wei & Sankardas Roy, Kansas State University.        
-All rights reserved. This program and the accompanying materials      
-are made available under the terms of the Eclipse Public License v1.0 
-which accompanies this distribution, and is available at              
-http://www.eclipse.org/legal/epl-v10.html                             
-*/
-package org.sireum.jawa.test.pilar
-
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.sireum.jawa.test.framework.pilar.PilarTestFramework
-import org.sireum.amandroid.example.interprocedural.PilarExamples
-import org.sireum.util.FileUtil
-
-/**
- * @author <a href="mailto:fgwei@k-state.edu">Fengguo Wei</a>
- */
-@RunWith(classOf[JUnitRunner])
-class PilarTest extends PilarTestFramework {
-		PilarExamples.goodModelFiles.
-		foreach { fileUri =>
-		  Analyzing title FileUtil.filename(fileUri) file fileUri
-		}
-}
+///*
+//Copyright (c) 2013-2014 Fengguo Wei & Sankardas Roy, Kansas State University.        
+//All rights reserved. This program and the accompanying materials      
+//are made available under the terms of the Eclipse Public License v1.0 
+//which accompanies this distribution, and is available at              
+//http://www.eclipse.org/legal/epl-v10.html                             
+//*/
+//package org.sireum.jawa.test.pilar
+//
+//import org.junit.runner.RunWith
+//import org.scalatest.junit.JUnitRunner
+//import org.sireum.jawa.test.framework.pilar.PilarTestFramework
+//import org.sireum.amandroid.example.interprocedural.PilarExamples
+//import org.sireum.util.FileUtil
+//
+///**
+// * @author <a href="mailto:fgwei@k-state.edu">Fengguo Wei</a>
+// */
+//@RunWith(classOf[JUnitRunner])
+//class PilarTest extends PilarTestFramework {
+////		PilarExamples.goodModelFiles.
+////		foreach { fileUri =>
+////		  Analyzing title FileUtil.filename(fileUri) file fileUri
+////		}
+//}
