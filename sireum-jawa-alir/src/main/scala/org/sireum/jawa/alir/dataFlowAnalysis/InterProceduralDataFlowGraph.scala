f2d5a4e35b4d973c74d396748e513485aa56f036
Fengguo Wei <fgwei@USF-Gold-Wifi-226-2-44.laptops.usf.edu>
2016-02-26 18:53:03 -0500

    OCD

--- sireum-jawa-alir/src/main/scala/org/sireum/jawa/alir/dataFlowAnalysis/InterProceduralDataFlowGraph.scala
+++ sireum-jawa-alir/src/main/scala/org/sireum/jawa/alir/dataFlowAnalysis/InterProceduralDataFlowGraph.scala
@@ -24,4 +24,4 @@ import org.sireum.jawa.alir.pta.PTAResult
  * @author <a href="mailto:fgwei@k-state.edu">Fengguo Wei</a>
  * @author <a href="mailto:sroy@k-state.edu">Sankardas Roy</a>
  */ 
-case class InterProceduralDataFlowGraph(icfg : InterproceduralControlFlowGraph[ICFGNode], ptaresult : PTAResult)
+case class InterProceduralDataFlowGraph(icfg: InterproceduralControlFlowGraph[ICFGNode], ptaresult: PTAResult)
